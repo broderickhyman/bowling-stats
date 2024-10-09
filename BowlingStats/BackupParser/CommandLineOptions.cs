72312539a6b66934f2cd038c0794dd43c91d55bf
Broderick Hyman <broderickhyman@gmail.com>
2024-10-09 18:18:05 -0400

    Setup a parsing console project

---
+++ BowlingStats/BackupParser/CommandLineOptions.cs
@@ -0,0 +1,10 @@
+﻿using CommandLine;
+
+namespace BackupParser
+{
+  internal class CommandLineOptions
+  {
+    [Option('p', "path", Required = true, HelpText = "Path to the .pinpal file")]
+    public string? Path { get; set; }
+  }
+}
---
+++ BowlingStats/BackupParser/Program.cs
@@ -0,0 +1,51 @@
+﻿using BackupParser;
+using CommandLine;
+
+Parser
+  .Default
+  .ParseArguments<CommandLineOptions>(args)
+  .WithParsed(Execute);
+return;
+
+void Execute(CommandLineOptions options)
+{
+  Console.WriteLine($"Path: {options.Path}");
+
+  using var readStream = File.OpenRead(options.Path);
+  var newFileName = Path.ChangeExtension(options.Path, ".db");
+  var startPosition = FindStartPosition(readStream);
+  if (startPosition < 0)
+  {
+    throw new Exception("Could not parse the file");
+  }
+
+  using var writeStream = File.OpenWrite(newFileName);
+  readStream.Seek(startPosition, SeekOrigin.Begin);
+  readStream.CopyTo(writeStream);
+}
+
+long FindStartPosition(FileStream readStream)
+{
+  var currentByte = readStream.ReadByte();
+  const string searchString = "SQLite format 3";
+  var currentSearchIndex = 0;
+  while (currentByte >= 0)
+  {
+    var character = (char)currentByte;
+    if (character == searchString[currentSearchIndex])
+    {
+      currentSearchIndex++;
+    }
+    else if (currentSearchIndex > 0)
+    {
+      currentSearchIndex = 0;
+    }
+    if (currentSearchIndex >= searchString.Length)
+    {
+      return readStream.Position - searchString.Length;
+    }
+    currentByte = readStream.ReadByte();
+  }
+
+  return -1;
+}
