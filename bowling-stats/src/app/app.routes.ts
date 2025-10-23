cb817247a084c77b724edb3db26dbe1714e39795
Broderick Hyman <broderickhyman@gmail.com>
2025-10-22 23:31:54 -0400

    Initial upload page

--- bowling-stats/src/app/app.routes.ts
+++ bowling-stats/src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { HomePage } from './home-page/home-page.component';
+import { UploadPage } from './upload-page/upload-page.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: HomePage,
+  },
+  {
+    path: 'upload',
+    component: UploadPage,
   },
 ];
---
+++ bowling-stats/src/app/upload-page/upload-page.component.ts
@@ -0,0 +1,7 @@
+import { Component, Input, inject, signal } from '@angular/core';
+
+@Component({
+  selector: 'upload-page',
+  templateUrl: './upload-page.component.html',
+})
+export class UploadPage {}
