2de63bd3e69e2fefadaa8cae67b8f69620c583a4
Broderick Hyman <broderickhyman@gmail.com>
2025-10-28 08:35:02 -0400

    Pin card displaying pins

--- bowling-stats/src/app/shared/components/pin-card.component.ts
+++ bowling-stats/src/app/shared/components/pin-card.component.ts
@@ -7,24 +7,46 @@ import { Component, input } from '@angular/core';
 export class PinCard {
   value = input.required<number>();
   pinData: PinDisplayData[] = [];
+  width = 0;
+  height = 0;
 
-  ngOnInit(){
+  ngOnInit() {
+    const val = this.value();
     // console.log(this.value());
-    let xOffset = 10;
-    // let yOffset = 0;
-    for(let pin = 0; pin < 10; pin++){
-    this.pinData.push({
-        x: xOffset,
-        y: 10,
-       fill: 'green'
-      });
-      xOffset += 30;
+    // Units are cm
+    const radius = 12.1 / 2;
+    let columnSpacing = 30.5;
+    let rowSpacing = 52.7 / 2;
+    let xOffset = radius;
+    let yOffset = radius;
+    let columnCount = 4;
+    let pin = 7;
+    while (columnCount > 0) {
+      xOffset = radius + (4 - columnCount) * (columnSpacing / 2);
+      for (let column = 0; column < columnCount; column++) {
+        const filled = ((val >> (pin - 1)) & 1) == 1;
+        this.pinData.push({
+          x: xOffset,
+          y: yOffset,
+          fill: filled ? 'green' : 'gray',
+          radius: radius,
+        });
+        xOffset += columnSpacing;
+        pin++;
+      }
+      pin -= columnCount;
+      yOffset += rowSpacing;
+      columnCount--;
+      pin -= columnCount;
     }
+    this.width = this.pinData.reduce((max, v) => Math.max(max, v.x), 0) + radius;
+    this.height = this.pinData.reduce((max, v) => Math.max(max, v.y), 0) + radius;
   }
 }
 
 export interface PinDisplayData {
   x: number;
-  y : number;
-  fill : string;
+  y: number;
+  radius: number;
+  fill: string;
 }
