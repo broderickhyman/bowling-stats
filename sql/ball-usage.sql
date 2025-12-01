86537c004901c2c13ba61aaf5a835f5b669f1466
Broderick Hyman <broderickhyman@gmail.com>
2025-12-01 14:18:00 -0500

    Misc ball usage

--- sql/ball-usage.sql
+++ sql/ball-usage.sql
@@ -4,6 +4,7 @@ b.pk
 , count(*) / 10 as 'Games'
 , ROUND(avg(f.scores & 15), 2) as 'Average'
 , STRFTIME('%m/%d/%Y', DATETIME(min(w.date), 'unixepoch')) as 'Date'
+-- , min(w.date)
 from ball b
 inner join frame f on f.ballFk = b.pk
 inner join week w on w.pk = f.weekFk
@@ -11,6 +12,7 @@ inner join week w on w.pk = f.weekFk
 where b.flags is null
 and f.flags & 1
 and f.leagueFk > 0
+and w.date > 1742248478
 
 group by
 b.pk
